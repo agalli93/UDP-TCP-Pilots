c674c0d9f444c2b30363f7b6ea41ba53879a23fd
agalli93 <agalli93@gmail.com>
2014-11-07 20:28:37 -0500

    Added in file creation and storage of received data from the UDP Server, file writing needs work

--- UDP_server.java
+++ UDP_server.java
@@ -1,10 +1,25 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.Path;
+import java.nio.file.FileSystemException;
+import java.nio.file.StandardOpenOption;
 import java.net.*;
+
 
 class UDPServer
 {
    public static void main(String args[]) throws Exception
    {
+      //Create a new file to store the data taken in by the server
+      Path file_path = Paths.get("C:\\users\\gallia\\dropbox\\linux\\Sim_data_dir\\Sim_data.txt");
+      boolean test = false;
+      int file_num = 0;
+      while (Files.exists(file_path)){
+         file_path = Paths.get("C:\\users\\gallia\\dropbox\\linux\\Sim_data_dir\\Sim_data" + ++file_num + ".txt");
+      }
+      Files.createFile(file_path); // Once the next available file name has been found, create it
+
       DatagramSocket serverSocket = new DatagramSocket(9876);
       while(true)
       {
@@ -14,6 +29,7 @@ class UDPServer
          serverSocket.receive(receivePacket);
          String sentence = new String( receivePacket.getData());
          System.out.println("RECEIVED: " + sentence);
+         Files.write(file_path, receiveData, StandardOpenOption.APPEND);
          InetAddress IPAddress = receivePacket.getAddress();
          int port = receivePacket.getPort();
          sendData = sentence.getBytes();
@@ -22,3 +38,7 @@ class UDPServer
       }
    }
 }
+/*
+Current problem is that when the byte array is written into the file, all 1024 bytes are written, regardless of their value. Need to find a way
+to determine how many bytes to actually record. X-plane might have a limit so we could just use that. For now, possibly binary searching the last
+bit. */
