91ef3bf3ffe06e382f092a4563caf5e417a235fa
agalli93 <agalli93@gmail.com>
2014-12-01 20:27:14 -0500

    Added functionality to receive multiple data groups from X-Plane

--- UDP_TCPConverter.java
+++ UDP_TCPConverter.java
@@ -26,43 +26,60 @@ class UDP_TCPConverter
       //Receiving socket opening and date creation
       DatagramSocket serverSocket = new DatagramSocket(9876);
       Date date = new Date();
-      SimpleDateFormat ft = new SimpleDateFormat (":yyyy-MM-dd hhmmssSSSZ");
+      SimpleDateFormat ft = new SimpleDateFormat (":yyyy-MM-dd hhmmssSSSZ:");
+
+      //Verify that data is ready to be transmitted to pilots
+      //**Request from user if they are ready or not**
+
+      //EDIT: Number of data streams
+      int num_data_streams = 4;
+      String header = "HEADER";
+      //end EDIT
+
+      //Send header to PILOTS
+      Socket headerSocket = new Socket("localhost", 6789);
+      DataOutputStream headerToServer = new DataOutputStream(headerSocket.getOutputStream());
+      headerToServer.writeBytes( header + '\n');
+      headerSocket.close();
+
+      //Begin Receiving Data
       while(true)
       {
          //Begin Reciving Data over UDP
-         byte[] receiveData = new byte[41]; //Number of bytes represents size of buffer in chars
+         byte[] receiveData = new byte[41+36*(num_data_streams-1)]; //Number of bytes represents size of buffer in chars
          DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
          serverSocket.receive(receivePacket);
-         String sentence = new String( receivePacket.getData());
-         System.out.println("Received data");
-         Files.write(file_path, receiveData, StandardOpenOption.APPEND);//Writing received data to a file
-            /*Sending back confirmation to the client
-            InetAddress IPAddress = receivePacket.getAddress();
-            int port = receivePacket.getPort();
-            sendData = sentence.getBytes();
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-            serverSocket.send(sendPacket);*/
          //End Receivng data over UDP
 
          //Get the current date and time since the data has been received
          date = new Date();
+         String output = ft.format(date);
 
-         //Convert data from X-Plane to PILOTS Format
-         String output = "";
-         for(int i=0; i<8; ++i){
-            byte[] test_array = {receiveData[4*i+9], receiveData[4*i+10], receiveData[4*i+11], receiveData[4*i+12]};
+         //Convert data from X-Plane format to PILOTS Format
+         for (int data_group = 0; data_group<num_data_streams; ++data_group) //For each data stream
+         {
+            int xplane_index = receiveData[5+36*data_group];
+            System.out.println("Index: " + xplane_index);
+            for(int i=0; i<8; ++i)
+            {
+               byte[] float_bytes = {receiveData[4*i+36*data_group+9], receiveData[4*i+36*data_group+10],
+                receiveData[4*i+36*data_group+11], receiveData[4*i+36*data_group+12]};
+               //Testing
+                  // byte[] test1_array = {(byte)0x00, 0x40, (byte)0xF6, (byte)0x42};
+                  //AB, 67, 51, BF works for Little Endian
+                  //00, 40, F6, 42 also works for little endian
+               float converted_number = ByteBuffer.wrap(float_bytes).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+               output = (output + Float.toString(converted_number) + ",");
+               // System.out.println("Float: " + converted_number);
+            }
             //Testing
-               // byte[] test1_array = {(byte)0x00, 0x40, (byte)0xF6, (byte)0x42};
-               //AB, 67, 51, BF works for Little Endian
-               //00, 40, F6, 42 also works for little endian
-            float converted_number = ByteBuffer.wrap(test_array).order(ByteOrder.LITTLE_ENDIAN).getFloat();
-            output = (output + Float.toString(converted_number) + ",");
-            // System.out.println("Float: " + converted_number);
+            output = output + "||";
          }
+         //System and file outputs
          System.out.println(ft.format(date)+":"+output);
+         Files.write(file_path, output.getBytes(), StandardOpenOption.APPEND);
 
-
-         //Begin Sending data over TCP/IP
+         //Begin Sending received and parsed data over TCP/IP
          Socket clientSocket = new Socket("localhost", 6789);
          DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
          outToServer.writeBytes(output + '\n');
