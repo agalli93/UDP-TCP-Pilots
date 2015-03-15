5006b6d85eaac6fdddff9dc84f10eade3c73247c
agalli93 <agalli93@gmail.com>
2015-03-15 19:26:05 +0800

    Added debug function, fixed files not creating properly, and put conversion into a function

--- UDP and TCP Testing/UDP_client.java
+++ UDP and TCP Testing/UDP_client.java
@@ -6,26 +6,53 @@ class UDPClient
    public static void main(String args[]) throws Exception
    {
       while(true){
-         // BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+         BufferedReader inFromUser =
+         new BufferedReader(new InputStreamReader(System.in));
          DatagramSocket clientSocket = new DatagramSocket();
-         InetAddress IPAddress = InetAddress.getByName("hw1-b00");
-         byte[] sendData = new byte[512];
-         byte[] receiveData = new byte[512];
-         String sentence = "Alessandro Galli, FEP2, 10.27.110.70"; //inFromUser.readLine();
-
+         InetAddress IPAddress = InetAddress.getByName("localhost");
+         byte[] sendData = new byte[41];
+         byte[] receiveData = new byte[41];
+         String sentence = inFromUser.readLine();
          sendData = sentence.getBytes();
-         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 17);
+         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
          clientSocket.send(sendPacket);
 
-         /* Receiving confirmation of arrival from the server with the message*/
-         DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-         clientSocket.receive(receivePacket);
-         String modifiedSentence = new String(receivePacket.getData());
-         System.out.println("FROM SERVER:" + modifiedSentence);
-
-
+            /* Receiving confirmation of arrival from the server with the message
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+            String modifiedSentence = new String(receivePacket.getData());
+            System.out.println("FROM SERVER:" + modifiedSentence);
+            */
          clientSocket.close();
-         break;
       }
    }
 }
+
+// class UDPClient
+// {
+//    public static void main(String args[]) throws Exception
+//    {
+//       while(true){
+//          // BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+//          DatagramSocket clientSocket = new DatagramSocket();
+//          InetAddress IPAddress = InetAddress.getByName("hw1-b00");
+//          byte[] sendData = new byte[512];
+//          byte[] receiveData = new byte[512];
+//          String sentence = "Alessandro Galli, FEP2, 10.27.110.70"; //inFromUser.readLine();
+
+//          sendData = sentence.getBytes();
+//          DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 17);
+//          clientSocket.send(sendPacket);
+
+//          /* Receiving confirmation of arrival from the server with the message*/
+//          DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+//          clientSocket.receive(receivePacket);
+//          String modifiedSentence = new String(receivePacket.getData());
+//          System.out.println("FROM SERVER:" + modifiedSentence);
+
+
+//          clientSocket.close();
+//          break;
+//       }
+//    }
+// }
